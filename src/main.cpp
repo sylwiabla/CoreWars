4acca14647b36117f40f79ff38f97189d1b82993
sylwiabla <sylwia.blach404@gmail.com>
2017-04-23 23:14:28 +0200

    fix: simple test

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 
 int main() {
-    Scanner::getInstance();
     std::string temp = "";
     Scanner::getInstance().setSourceManager("/home/sylwia/ClionProjects/RedcodeInterpreter/test/test");
 
@@ -13,7 +12,7 @@ int main() {
         temp = Scanner::getInstance().getToken();
         std::cout << temp << std::endl;
     }
-    while((temp != "End of source file") && (temp != "Error: too long identifier name"));
+    while((temp != "Too long identifier") && (temp != "EOF"));
 
     return 0;
 }
