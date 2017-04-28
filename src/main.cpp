0931a7acce7517da22f56eadd1663651b0656a0e
sylwiabla <sylwia.blach404@gmail.com>
2017-04-28 07:57:59 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,6 @@
 using namespace std;
 
 int main() {
-    std::string temp = "";
-    Scanner::getInstance().setSourceManager("/home/sylwia/ClionProjects/RedcodeInterpreter/test/test");
-
-    do
-    {
-        temp = Scanner::getInstance().getToken();
-        std::cout << temp << std::endl;
-    }
-    while((temp != "Too long identifier") && (temp != "EOF"));
 
     return 0;
 }
